20e92b4a4532d815117477bfcbe36633d0e19af0
wali <sistemasigb@gmail.com>
2023-03-16 13:25:57 -0500

    subiendo proyecto

---
+++ src/main/java/com/wali/WaliApplication.java
@@ -0,0 +1,13 @@
+package com.wali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wali/WaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
